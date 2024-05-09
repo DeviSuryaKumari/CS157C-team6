da4231a6b63aa49c8936f3bc180aecc3b157b56c
odiaz95012 <odiaz95012@gmail.com>
2024-05-09 15:06:42 -0700

    Adding what we have so far in movie controller

--- popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/MovieRepository.java
+++ popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.cs157c.popcornpicks.repository;
 
+import com.cs157c.popcornpicks.model.DirectorEntity;
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
@@ -11,11 +12,22 @@ import org.springframework.data.neo4j.repository.query.Query;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Objects;
+
 public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity, String> {
 
 	@Query("MATCH (m:Movie) RETURN m ORDER BY m.rating DESC LIMIT 50")
 	Flux<MovieEntity> getTop50Movies();
 
+	@Query("MATCH (d:Director)-[:DIRECTED]->(m:Movie {title: $title}) RETURN d")
+	Flux<DirectorEntity> getDirectorByMovieTitle(String title);
+
+	@Query("MATCH (a:Actor)-[:ACTED_IN]->(m:Movie {title: $title}) RETURN a")
+	Flux<DirectorEntity> getActorsByMovieTitle(String title);
+
+	@Query("MATCH (m:Movie) UNWIND m.genres AS genres WITH DISTINCT genres RETURN COLLECT(genres)")
+	Flux<List<?>> getGenres();
 	Mono<MovieEntity> findByTitle(String title);
 
 	Mono<Void> deleteByTitle(String title);
