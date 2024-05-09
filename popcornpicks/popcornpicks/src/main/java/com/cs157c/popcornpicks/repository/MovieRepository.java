53cf1f724308c23e95c9999c8192257a54b37df3
sunidhithakker <99764121+sunidhithakker@users.noreply.github.com>
2024-05-09 00:41:37 -0700

    added find movie by id

--- popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/MovieRepository.java
+++ popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/MovieRepository.java
@@ -1,12 +1,17 @@
 package com.cs157c.popcornpicks.repository;
 
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import com.cs157c.popcornpicks.model.MovieEntity;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity, String> {
+public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity, Integer> {
 	Mono<MovieEntity> findOneByTitle(String title);
 	Mono<Void> deleteByTitle(String title);
+	Mono<MovieEntity> findOneById(int id);
+	@Query("MATCH (m:Movie) RETURN m ORDER BY m.rating DESC LIMIT 50")
+    Flux<MovieEntity> getTop50Movies();
 }
