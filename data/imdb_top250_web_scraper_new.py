82f183099764b616965b7950a39a4619bf77df7d
Devi Akula <devisurya789@gmail.com>
2024-05-09 01:54:47 -0700

    1. Updated web scraper 2. Added Cypher to bulk import movies, actors, directors, users and to create relationships among them

--- data/imdb_top250_web_scraper_new.py
+++ data/imdb_top250_web_scraper_new.py
@@ -1,32 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-
 import pandas as pd
 
 url = 'https://www.imdb.com/chart/top/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
-response = requests.get(url, headers=headers, timeout=(5.05, 22))
-print(f'HTTP GET status code for base url https://www.imdb.com/chart/top/: {response.status_code}')
+response = requests.get(url, headers = headers, timeout = (5.05, 22))
 ## Status code: 200 means everything is OK
+print(f'HTTP GET status code for base url https://www.imdb.com/chart/top/: {response.status_code}')
+
 
 page_contents = response.text
 soup = BeautifulSoup(page_contents, 'html.parser')
 
 movie_lists_html = soup.find_all('li', class_='ipc-metadata-list-summary-item')
-print(f"Number of movies: {len(soup.find_all('li', class_='ipc-metadata-list-summary-item'))}")
+print(f"Number of movies: {len(movie_lists_html)}")
 
-## get the names of the movies 
+def sep(txt):
+    rating, num_votes = txt.split('\xa0')[0], txt.split('\xa0')[1][1:-1]
+    return rating, num_votes
+
 movie_names = []
-
-for i in range(250):
-    movie_names.append(movie_lists_html[i].find('div', class_='cli-children').find('a').find('h3').text.split('.')[1].strip())
-    
+posters = []
+rating = []
+num_votes = []
 year_of_release = []
 time_span = []
 certi_type = []
 
 for i in range(250):
+    movie_names.append(movie_lists_html[i].find('div', class_='cli-children').find('a').find('h3').text.split('.')[1].strip())
+    posters.append(movie_lists_html[i].find('img', class_='ipc-image')['src'])
+    res = sep(movie_lists_html[i].find('div', class_='cli-children').find('span', class_='ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating').text)
+    rating.append(res[0])
+    num_votes.append(res[1])
+
     try:
         year_of_release.append(movie_lists_html[i].find('div', class_='cli-children').find('div', class_='cli-title-metadata').find_all('span', class_='cli-title-metadata-item')[0].text)
     except IndexError:
@@ -40,39 +48,29 @@ for i in range(250):
     except IndexError:
         certi_type.append('')
 
-def sep(txt):
-    rating, num_votes = txt.split('\xa0')[0], txt.split('\xa0')[1][1:-1]
-    return rating, num_votes
-
-rating = []
-num_votes = []
-
-for i in range(250):
-    res = sep(movie_lists_html[i].find('div', class_='cli-children').find('span', class_='ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating').text)
-    rating.append(res[0])
-    num_votes.append(res[1])
-
 
 # Visiting each movie url to collect movie description, genres, director, actor details
 base_url = 'https://www.imdb.com'
 
 genres = []
-directors = []
 plots = []
+
+directors = []
+movies_d = []
+
 actors = []
+movies_a = []
 
 for i in range(250):
     movie_link = base_url + movie_lists_html[i].find('div', class_='cli-children').find('a')['href']
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(movie_link, headers=headers, timeout=(5.05, 22))
+    response = requests.get(movie_link, headers = headers, timeout = (5.05, 22))
 
     # print(f'status code for movie url: {response.status_code}')
     parse = BeautifulSoup(response.text, 'html.parser')
 
     genre = parse.find('div', class_='ipc-chip-list--baseAlt ipc-chip-list')
     director = parse.find_all('li', class_='ipc-metadata-list__item')[0]
-    star = parse.find_all('li', class_='ipc-metadata-list__item')[2]
+    actor = parse.find_all('li', class_='ipc-metadata-list__item')[2]
 
     plot = parse.find('p', attrs={'data-testid': 'plot'})
     plot_text = plot.find('span', attrs={'data-testid': 'plot-xs_to_m'}).text
@@ -82,34 +80,47 @@ for i in range(250):
         genres_list.append(item.text)
     genres_list = '|'.join(genres_list)
 
-    director_list = []
     for item in director.find_all('a', class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link'):
-        director_list.append(item.text)
-    director_list = '|'.join(director_list)
+        directors.append(item.text)
+        movies_d.append(movie_names[i])
 
-    stars_list = []
-    for item in star.find_all('a', class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link'):
-        stars_list.append(item.text)
-    stars_list = '|'.join(stars_list)
+    for item in actor.find_all('a', class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link'):
+        actors.append(item.text)
+        movies_a.append(movie_names[i])
         
     genres.append(genres_list)
-    directors.append(director_list)
     plots.append(plot_text)
-    actors.append(stars_list)
 
-data = {
+movie_data = {
     'movie_title': movie_names,
+    'poster': posters,
     'released_year': year_of_release,
     'duration': time_span,
     'certificate_type': certi_type,
     'rating': rating,
     'rating_count': num_votes,
-    'directors': directors,
     'genres': genres,
-    'actors': actors,
     'plot': plots
 }
 
-movie_df = pd.DataFrame(data)
-movie_df.to_csv('Top-250-movies.csv')
+actor_data = {
+    'actor': actors,
+    'movie': movies_a
+}
+
+director_data = {
+    'director': directors,
+    'movie': movies_d
+}
+
+movie_df = pd.DataFrame(movie_data)
+actor_df = pd.DataFrame(actor_data)
+director_df = pd.DataFrame(director_data)
+
+movie_df.to_csv('movies.csv', index = False)
+actor_df.to_csv('actors.csv', index = False)
+director_df.to_csv('directors.csv', index = False)
+
 print(movie_df)
+# print(actor_df)
+# print(director_df)
