5df50c1f99fe4cca4510da3f228811e642a555c2
sunidhithakker <99764121+sunidhithakker@users.noreply.github.com>
2024-05-09 01:50:38 -0700

    API for users to like and dislike list of movies by id

--- popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/UserRepository.java
+++ popcornpicks/popcornpicks/src/main/java/com/cs157c/popcornpicks/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.cs157c.popcornpicks.repository;
 
+import com.cs157c.popcornpicks.model.MovieEntity;
 import com.cs157c.popcornpicks.model.UserEntity;
+
+import java.util.List;
+
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,4 +16,9 @@ public interface UserRepository extends ReactiveNeo4jRepository<UserEntity, Stri
     Mono<UserEntity> findByUsername(String username);
     Flux<UserEntity> findUsersFollowersByUsername(String username);
 
+    @Query("MATCH (u:User {username: $username}), (m:Movie) WHERE ID(m) IN $movieIds MERGE (u)-[:LIKED]->(m)")
+    Mono<UserEntity> likeMovies(String username, List<Integer> movieIds);
+
+    @Query("MATCH (u:User {username: $username}), (m:Movie) WHERE ID(m) IN $movieIds MERGE (u)-[:DISLIKED]->(m)")
+    Mono<UserEntity> dislikeMovies(String username, List<Integer> movieIds);
 }
